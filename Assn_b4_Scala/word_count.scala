91d384bcb5cde4e6ce03a171c326bd462c0bf297
A N U J  M U T H A <mutha.am16@gmail.com>
2022-09-07 17:37:38 +0530

    Update word_count.scala

--- Assn_b4_Scala/word_count.scala
+++ Assn_b4_Scala/word_count.scala
@@ -7,3 +7,6 @@ val lines = spark.readStream.format("socket").option("host","localhost").option(
 val words = lines.as[String].flatMap(_.split(" "))
 val wordCounts = words.groupBy("value").count()
 val query = wordCounts.writeStream.outputMode("complete").format("console").start()
+
+
+
